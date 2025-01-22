c99550a3eea21a02d379e17c1654f2107e184f0d
Andrea Fumagalli <160780597+fumaghe@users.noreply.github.com>
2025-01-22 13:16:18 +0100

    Translation 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/PartyGame/', // <-- Nome esatto del repository
+  base: '/PartyGame/', // <-- Assicurati che sia il nome esatto del repository
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    outDir: 'dist', // Specifica la cartella di output
+  }
 });
